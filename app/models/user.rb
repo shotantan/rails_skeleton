3f5e25a9cfceefb38407a3fcb5dc2f0b12475df9
shotantan <shota.nakano@manycolors-inc.com>
2015-03-03 16:31:28 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
-
+  has_and_belongs_to_many :sensors
+  
   def has_role?(name)
     self.roles.where(name: name).length > 0
   end
+
+  def has_sensor?(mac)
+    self.sensors.where(mac: mac).length > 0
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
